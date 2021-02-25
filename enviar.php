72e621518cf9e744c9447023c029e42849ce3f1b
Luandrada <luandrada12@live.com>
2021-02-25 15:24:52 -0300

    primer commit

---
+++ enviar.php
@@ -0,0 +1,26 @@
+<?php
+    ///llamando a los campus
+    $nombre = $_POST['nombre'];
+    $empresa= $_POST['empresa'];
+    $email = $_POST['email'];
+    $telefono = $_POST['telefono'];
+    $ciudad = $_POST['ciudad'];
+    $mensaje = $_POST['mensaje'];
+
+    /// Datos para el correo
+    $destinatario ="luandrada12@live.com";
+    $asunto ="Contacto desde pagina web";
+
+    $carta = "De: $nombre \n";
+    $carta .= "Empresa: $empresa \n";
+    $carta .= "Email: $email \n";
+    $carta .= "Tel.: $telefono \n";
+    $carta .= "Ciudad: $ciudad \n";
+    $carta .= "Mensaje: $mensaje \n";
+
+    ///// Enviando mensaje
+
+    mail($destinatario, $asunto, $carta);
+    header('Location:msje-envio.html')
+
+?>
